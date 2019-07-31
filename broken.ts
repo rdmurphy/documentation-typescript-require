26bc36e8e8860715d82c32c53428c3610878ca90
Ryan Murphy <rdmurphy@users.noreply.github.com>
2019-07-30 18:39:38 -0700

    Initial commit

---
+++ broken.ts
@@ -0,0 +1,12 @@
+/**
+ * Squares a number.
+ *
+ * @param n The number to square
+ * @example
+ * const { square } = require('project');
+ * 
+ * square(2);
+ */
+export function square(n: number) {
+  return n * n;
+}
---
+++ working.ts
@@ -0,0 +1,12 @@
+/**
+ * Squares a number.
+ *
+ * @param n The number to square
+ * @example
+ * import { square } from 'project';
+ * 
+ * square(2);
+ */
+export function square(n: number) {
+  return n * n;
+}
